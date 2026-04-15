8e4b45a2d30a7eacdbc975ea2b7181c22906bca1
afiya27taskeen-byte <afiya27taskeen@gmail.com>
2026-04-15 20:25:39 +0530

    Add files via upload

---
+++ if else example.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+youAresick=input("Enter yes or no:")
+if youAresick == "yes":
+    print("Go to doctor")
+else:
+    print("go and play football")
+
+
+# In[ ]:
+
+
+
+
